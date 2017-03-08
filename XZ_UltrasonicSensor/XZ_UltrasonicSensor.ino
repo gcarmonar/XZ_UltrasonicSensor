476144591746caa442d9b0b304a7bedcfe11d88d
Gerardo Carmona <gcarmonar@gmail.com>
2017-03-08 17:01:47 -0600

    Added some filtering

--- XZ_UltrasonicSensor/XZ_UltrasonicSensor.ino
+++ XZ_UltrasonicSensor/XZ_UltrasonicSensor.ino
@@ -62,16 +62,101 @@ private:
 };
 
 Ultrasonic UltLeft(3,2);
-//Ultrasonic UltRight(6,5);
+Ultrasonic UltRight(12,11);
+
+const int size = 10;		// For arrays
+const int sizeBin = 10;		// For binary arrays
+const int thresholdBin = 3;
+const int threshold = 140; 	// In cms
+
+float leftArray[size];
+float rightArray[size];
+float leftBinArray[size];
+float rightBinArray[size];
+float leftAverageArray[size];
+float rightAverageArray[size];
+int count = 0;
+int countBin = 0;
+
+
 
 void setup() {
 	// put your setup code here, to run once:
 	Serial.begin(9600);
 	
 }
-
+ 
 void loop() {
 	// put your main code here, to run repeatedly:
-  	Serial.println(UltLeft.GetCms());
-  	delay(1000);
+	
+	leftArray[count] = UltLeft.GetCms();
+	rightArray[count] = UltRight.GetCms();
+
+	leftAverageArray[count] = GetAverage(leftArray);
+	rightAverageArray[count] = GetAverage(rightArray);
+	
+	CalculateBinArray(leftAverageArray, leftBinArray);
+	CalculateBinArray(rightAverageArray, rightBinArray);
+
+
+	Serial.print(millis());
+	Serial.print(",");
+
+	Serial.print(leftArray[count]);
+	Serial.print(",");
+	Serial.print(rightArray[count]);
+	Serial.print(",");
+
+	Serial.print(leftAverageArray[count]);
+	Serial.print(",");
+	Serial.print(rightAverageArray[count]);
+	Serial.print(",");
+
+	Serial.print(DetectObject(leftBinArray));
+	Serial.print(",");
+	Serial.print(DetectObject(rightBinArray));
+	Serial.println("");
+
+
+	
+
+	count++;
+	if (count == size){
+		count = 0;
+	}
+   
+  	//delay(20);
+}
+
+float GetAverage(float data[]){
+	float average=0;
+
+	for (int i = 0; i < size; i++)
+		average += data[i];
+
+	return (average / size);
+}
+
+void CalculateBinArray(float *rawArray, float *binArray){
+	for (int i = 0; i < size; i++){
+		if (rawArray[i] < threshold){
+			binArray[i] = 1;
+		}else{
+			binArray[i] = 0;
+		}
+	}
+}
+
+int DetectObject(float *binArray){
+	int sum=0;
+
+	for (int i = 0; i < size; i++)
+		sum += binArray[i];
+
+	if (sum  > ((size/2)-1)){
+		return 1;
+	}
+
+	return 0;
+
 }
